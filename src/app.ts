4208fc173309ee36aaa1a9e09b386e8769d54d82
MarcoBagnasco <bagna.hbk@gmail.com>
2021-10-12 09:40:38 +0200

    finish

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+/************************************************ Lesson 146 - 147 ************************************************/
+import {ProjectInput} from './components/project-input';
+import {ProjectList} from './components/project-list';
+
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
